8115e9fcc2c3fc82357fcfcc492e1e555caee6c4
songmh <songmh2010@163.com>
2020-03-14 22:27:51 +0800

    删除B中的example

--- ModuleB/Example/Pods/Target Support Files/ModuleB/ModuleB-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ModuleB : NSObject
-@end
-@implementation PodsDummy_ModuleB
-@end
--- ModuleB/Example/Pods/Target Support Files/ModuleC/ModuleC-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ModuleC : NSObject
-@end
-@implementation PodsDummy_ModuleC
-@end

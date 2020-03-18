4bff8203a28f949d1be16d4641abefd5aee11805
songmh <songmh2010@163.com>
2020-03-18 22:07:08 +0800

    Module 添加控制器

---
+++ ModuleA/ModuleA_VC.swift
@@ -0,0 +1,31 @@
+//
+//  ModuleA_VC.swift
+//  ModuleA_Example
+//
+//  Created by s on 2020/3/18.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ModuleA_VC: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        self.title = NSStringFromClass(self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
